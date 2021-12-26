9f8db2aaf94093737282f9951fea6bdb4743f969
jalbertomr <jalbertomr@yahoo.com>
2021-12-26 16:58:56 -0600

    adding extra locations for web static html files by java add properties to app

--- src/main/java/com/bext/springbootwebstatic/SpringbootwebstaticApplication.java
+++ src/main/java/com/bext/springbootwebstatic/SpringbootwebstaticApplication.java
@@ -3,11 +3,17 @@ package com.bext.springbootwebstatic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class SpringbootwebstaticApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootwebstaticApplication.class, args);
+        SpringApplication application = new SpringApplication(SpringbootwebstaticApplication.class);
+        Properties props = new Properties();
+        props.setProperty("spring.web.resources.static-locations", "classpath:/static_extra/,classpath:/static_extra2/");
+        application.setDefaultProperties(props);
+        application.run(args);
     }
 
 }
