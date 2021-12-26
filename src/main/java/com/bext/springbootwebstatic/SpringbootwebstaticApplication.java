12da2a96abb043675390ca5878b82e0fa80cf578
jalbertomr <jalbertomr@yahoo.com>
2021-12-26 16:09:19 -0600

    first commit, web static default locations

---
+++ src/main/java/com/bext/springbootwebstatic/SpringbootwebstaticApplication.java
@@ -0,0 +1,13 @@
+package com.bext.springbootwebstatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebstaticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwebstaticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bext/springbootwebstatic/SpringbootwebstaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bext.springbootwebstatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebstaticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
